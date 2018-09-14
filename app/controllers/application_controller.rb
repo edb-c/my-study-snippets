c4ece7f2303982229df7028086efbd305008f352
edb-c <edb.cofresi@gmail.com>
2018-09-14 13:37:03 -0400

    Add Bootstrap index file code / images.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,16 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+# Required for logins and flash messages
+#    enable :sessions
+#    set :session_secret, "password_security"
+#    register Sinatra::Flash
+
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
 end
