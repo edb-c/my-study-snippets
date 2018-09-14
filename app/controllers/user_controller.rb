8ffc88dd9df99151219d3f4ad3c04e2e09b5c96d
edb-c <edb.cofresi@gmail.com>
2018-09-14 18:16:11 -0400

    Testing post logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,4 +5,13 @@ class UserController < ApplicationController
     erb :'users/login'
   end
 
+# Register form action - Creates new user
+# Instantiate new user based on signup form values
+
+#  get '/register' do
+#    user = User.new(params[:user])
+#    user.save
+#  end
+
+
 end  #/end class
