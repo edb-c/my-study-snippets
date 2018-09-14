a861d5468afa01d6d7aace7c6f594dcefadbfba2
edb-c <edb.cofresi@gmail.com>
2018-09-14 09:06:12 -0400

    Update 20180914130215_create_categories.rb
    changed snippet from string to text

--- db/migrate/20180914130215_create_categories.rb
+++ db/migrate/20180914130215_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :snippit
+      t.text :snippit
 
       t.timestamps null: false
     end
